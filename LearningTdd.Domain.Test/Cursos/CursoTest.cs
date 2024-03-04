cfa40a200dd431e29b7f6d8010ed4503dcb8665e
Hugo Falcao <hugocoelhof03@gmail.com>
2024-03-04 15:53:21 -0300

    Adicionar arquivos de projeto.

---
+++ LearningTdd.Domain.Test/Cursos/CursoTest.cs
@@ -0,0 +1,34 @@
+﻿using LearningTdd.Domain.Cursos;
+
+namespace LearningTdd.Domain.Test.Cursos
+{
+    public class CursoTest
+    {
+        //Criar um curso com os seguintes dados
+        //        Nome, Carga Horária, Publico Alvo e Valor
+
+        [Fact]
+        public void Curso_CriarCurso_RetornaCurso()
+        {
+            var CursoEsperado = new
+            {
+                Nome = "Informática Básica",
+                CargaHoraria = (double) 80,
+                PublicoAlvo = "Estudantes",
+                Valor = (decimal) 950.00
+            };
+
+            var curso = new Curso(
+                CursoEsperado.Nome,
+                CursoEsperado.CargaHoraria,
+                CursoEsperado.PublicoAlvo,
+                CursoEsperado.Valor
+            );
+
+            Assert.Equal(CursoEsperado.Nome, curso.Nome);
+            Assert.Equal(CursoEsperado.CargaHoraria, curso.CargaHoraria);
+            Assert.Equal(CursoEsperado.PublicoAlvo, curso.PublicoAlvo);
+            Assert.Equal(CursoEsperado.Valor, curso.Valor);
+        }
+    }
+}
---
+++ LearningTdd.Domain/Cursos/Curso.cs
@@ -0,0 +1,33 @@
+﻿namespace LearningTdd.Domain.Cursos
+{
+    public class Curso
+    {
+        public string Nome { get; private set; }
+        public double CargaHoraria { get; private set; }
+        public string PublicoAlvo { get; private set; }
+        public decimal Valor { get; private set; }
+
+        public Curso(string nome, double cargaHoraria, string publicoAlvo, decimal valor)
+        {
+            if (string.IsNullOrEmpty(nome))
+            {
+                throw new ArgumentException("Nome inválido");
+            }
+
+            if (cargaHoraria < 1)
+            {
+                throw new ArgumentException("Carga horária inválida");
+            }
+
+            if (valor < 1)
+            {
+                throw new ArgumentException("Valor inválido");
+            }
+
+            Nome = nome;
+            CargaHoraria = cargaHoraria;
+            PublicoAlvo = publicoAlvo;
+            Valor = valor;
+        }
+    }
+}
